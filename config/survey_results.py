7223890251714086570ab5dbcc4bafa421304e41
Ian Ivo <ianivo.pro@gmail.com>
2025-09-20 00:08:18 -0300

    Create survey_results.py

---
+++ config/survey_results.py
@@ -0,0 +1,11 @@
+{
+    'Melhoria no transporte': {'Sim': 3, 'Não': 97},
+    'Avaliação contínua': {'Sim': 0, 'Não': 100},
+    'Site de agendamento eficaz': {'Sim': 98, 'Não': 2},
+    'Problemas com transporte': {'Sim': 5, 'Não': 95},
+    'Horários convenientes': {'Sim': 0, 'Não': 100},
+    'Funcionários explicam bem': {'Sim': 92, 'Não': 8},
+    'Satisfação com tempo de espera': {'Sim': 37, 'Não': 63},
+    'Equipe acessível': {'Sim': 38, 'Não': 62},
+    'Sentimento de acolhimento': {'Sim': 43, 'Não': 57}
+}
